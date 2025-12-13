dd9082eb1f75c84f4c35387a5d8cf61c2ddd8a13
KevinHuang2079 <kevhuang@chapman.edu>
2025-12-12 23:32:37 -0800

    commit

--- backend/src/middleware/cors.js
+++ backend/src/middleware/cors.js
@@ -1,7 +1,7 @@
 const cors = require('cors');
 
 const corsOptions = {
-    origin: process.env.CORS_OPTION || 'http://localhost:3000',
+    origin: process.env.CLIENT_URL || 'http://localhost:3000',
     credentials: true,
 };
 
